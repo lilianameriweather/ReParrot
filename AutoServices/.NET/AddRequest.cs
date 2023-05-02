e76b8db27c5bda29fe312507d4f6c700b6a5108f
Liliana Meriweather <78009370+lilianameriweather@users.noreply.github.com>
2023-05-01 17:31:51 -0700

    Update AddRequest.cs

--- AutoServices/.NET/AddRequest.cs
+++ AutoServices/.NET/AddRequest.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Requests.AutoServices
+namespace Reparrot.Models.Requests.AutoServices
 {
     public class AutoServiceAddRequest
     {
