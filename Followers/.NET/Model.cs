c643a1172405a1ce8b19ec883b823f527cb2b438
Liliana Meriweather <78009370+lilianameriweather@users.noreply.github.com>
2023-05-01 17:38:03 -0700

    Update Model.cs

--- Followers/.NET/Model.cs
+++ Followers/.NET/Model.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Domain.Followers
+namespace Reparrot.Models.Domain.Followers
 {
     public class Follower
     {
